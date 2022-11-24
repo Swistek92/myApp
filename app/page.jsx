db35cd1057800349b18dd38a7186c167ab17b091
Piotrek <Swistekxd@gmail.com>
2022-11-24 23:48:04 +0100

    add

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,23 @@ import "./mainStyles.css";
 import Image from "next/image";
 import ja from "../public/ja.jpg";
 import styles from "./page.module.css";
+import Loading from "./Loading";
+import background from "../public/sunrise.JPG";
 export default function Page() {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <Image width={1280} height={960} src={background} />
+      <div className={styles.logoBox}>
+        <p className={styles.logo}>
+          <span>&#9774;</span>
+        </p>
+      </div>
+      <div className={styles.textBox}>
+        <h1 className={styles.heading}>
+          <span className={styles.headingName}>??????</span>
+          <span className={styles.headingSub}>is where life happens </span>
+        </h1>
+      </div>
+    </div>
+  );
 }
