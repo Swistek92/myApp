8a2056d8748093b93dd2938befa07928ae7a4bee
Piotrek <Swistekxd@gmail.com>
2022-12-15 23:27:42 +0100

    redirect

--- app/auth/signin/SignInComponent.tsx
+++ app/auth/signin/SignInComponent.tsx
@@ -17,8 +17,9 @@ const SignInComponent = ({ providers }: Props) => {
               className='bg-blue-500 mx-1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'
               onClick={() =>
                 signIn(e.id, {
-                  callbackUrl:
-                    process.env.VERCEL_URL || "http://localhost:3000",
+                  redirect: false,
+                  // callbackUrl:
+                  //   process.env.VERCEL_URL || "http://localhost:3000",
                 })
               }
             >
