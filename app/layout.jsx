716450b6b60c2a315aacf5c6659fb9b8cf6f65fd
Piotrek <Swistekxd@gmail.com>
2022-12-21 02:44:27 +0100

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,10 @@ import Header from "./Components/Header/Header";
 import styles from "./layout.module.css";
 import Wrappers from "./Components/Wrappers/Wrappers";
 import { unstable_getServerSession } from "next-auth";
-import { NextAuth } from "pages/api/auth/[...nextauth]";
+import NextAuth from "pages/api/auth/[...nextauth]";
 
 export default async function RootLayout({ children, ...props }) {
-  const session = await unstable_getServerSession(NextAuth);
-  console.log("server side", session);
+  let session = (session = await unstable_getServerSession(NextAuth));
 
   return (
     <html lang='en'>
