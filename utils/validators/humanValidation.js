96312bc7d529c1f71ccb81965a2de336bc351780
Piotrek <Swistekxd@gmail.com>
2023-05-18 00:51:48 +0200

    changes

---
+++ utils/validators/humanValidation.js
@@ -0,0 +1,13 @@
+async function validateHuman(token) {
+  const secret = process.env.BACKEND_RECAPTCHA_SECRET_KEY;
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+    {
+      method: "POST",
+    }
+  );
+  const data = await response.json();
+  return data.success;
+}
+
+export default validateHuman;
