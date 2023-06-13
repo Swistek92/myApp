4e33f4e126411e90c1d3dbf8b1d34d64719d3b7f
Piotrek <Swistekxd@gmail.com>
2023-06-13 06:16:10 +0200

    add policy

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./styles.module.css";
+import Link from "next/link";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>© Kopiowanie treści strony bez zgody autora zabronione.</p>
+      <p>
+        © Kopiowanie treści strony bez zgody autora zabronione.{" "}
+        <Link className={`next-link `} href='/policy'>
+          polityka prywatnosci
+        </Link>
+      </p>
     </div>
   );
 };
